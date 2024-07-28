5b36339b9d5428a0c6ad11eb01046cfebb6dcedf
MarzukhAsjad <marz.asjad00@gmail.com>
2024-07-28 22:49:43 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Serve static files from the 'images' directory
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Serve static files from the 'models' directory
+app.use('/models', express.static(path.join(__dirname, 'models')));
+
+// Serve static files from the 'src' directory
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+// Serve index.html from the root directory
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
